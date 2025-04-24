06588a560d4ced7eeb8a089a3d5999705fe925be
Georgi Toni Georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2025-04-24 21:25:53 +0300

    feat: start placing background and vault door game objects around the scene

--- src/scenes/VaultClosed.ts
+++ src/scenes/VaultClosed.ts
@@ -1,14 +1,34 @@
+import { Assets } from "pixi.js";
 import Scene from "../core/Scene";
+
+import Background from '../prefabs/Background';
+import DoorClosed from "../prefabs/DoorClosed";
+import DoorHandle from "../prefabs/DoorHandle";
 
 export default class VaultClosed extends Scene {
 
     name = "VaultClosed";
 
+    private mainBackgr!: Background;
+    private doorClosed!: DoorClosed;
+    private doorHandle!: DoorHandle;
+
 	async load() {
 
 		await this.utils.assetLoader.loadAssetsGroup("VaultClosed");
 
+        this.mainBackgr = new Background(Assets.get('bg'));
+        this.doorClosed = new DoorClosed(Assets.get('door'));
+        this.doorHandle = new DoorHandle(Assets.get('handle'));
 
+        this.doorClosed.anchor.set(0.5);
+        this.doorClosed.position.set(55, -35);
+
+        this.mainBackgr.resize(window.innerWidth, window.innerHeight);
+
+        this.doorClosed.addChild(this.doorHandle);
+        this.mainBackgr.addChild(this.doorClosed);
+        this.addChild(this.mainBackgr);
 	}
 
 	async start() {
