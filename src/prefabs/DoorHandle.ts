7a0f615417c9d789362cb066ec8cfc0f9f5acd69
Georgi Toni Georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2025-04-24 21:48:08 +0300

    feat: modified door handle class as a container for the door handle sprite and its shadow

--- src/prefabs/DoorHandle.ts
+++ src/prefabs/DoorHandle.ts
@@ -1,3 +1,17 @@
-import { Sprite } from "pixi.js";
+import { Assets, Sprite, Texture, Container } from "pixi.js";
 
-export default class DoorHandle extends Sprite { }
+export default class DoorHandle extends Container {
+
+    private handle!: Sprite;
+    private handleShadow!: Sprite;
+
+    constructor() {
+        super();
+        this.handle = new Sprite(Assets.get('handle'));
+        this.handle.anchor.set(0.5);
+        this.handleShadow = new Sprite(Assets.get('handleShadow'));
+        this.handleShadow.anchor.set(0.5);
+        this.handleShadow.position.set(20, 35);
+        this.addChild(this.handleShadow, this.handle);
+    }
+}
