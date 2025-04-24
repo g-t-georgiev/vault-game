86fa95e47a396ebe84e9e8bbef11e082fc270202
Georgi Toni Georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2025-04-24 21:23:58 +0300

    feat: added background game object

---
+++ src/prefabs/Background.ts
@@ -0,0 +1,15 @@
+import { Sprite } from "pixi.js";
+import { centerObjects } from "../utils/misc";
+
+export default class Background extends Sprite {
+
+    resize(width: number, height: number) {
+		const scaleByWidth = width / this.texture.width;
+		const scaleByHeight = height / this.texture.height;
+		const scaleFactor = Math.min(scaleByWidth, scaleByHeight);
+		this.width = this.texture.width * scaleFactor;
+		this.height = this.texture.width * scaleFactor;
+		this.scale.set(scaleFactor);
+		centerObjects(this);
+	}
+}
