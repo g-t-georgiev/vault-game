db81ba44109df25c1b4b70b4b37bd10de5629b6c
Georgi Toni Georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2025-04-24 22:16:48 +0300

    feat: added opened door container for the door and its shadow sprites

---
+++ src/prefabs/DoorOpened.ts
@@ -0,0 +1,17 @@
+import { Assets, Sprite, Container } from "pixi.js";
+
+export default class DoorOpened extends Container {
+
+    private door!: Sprite;
+    private doorShadow!: Sprite;
+
+    constructor() {
+        super();
+        this.door = new Sprite(Assets.get('doorOpen'));
+        this.door.anchor.set(0.5);
+        this.doorShadow = new Sprite(Assets.get('doorOpenShadow'));
+        this.doorShadow.anchor.set(0.5);
+        this.doorShadow.position.set(85, 50);
+        this.addChild(this.doorShadow, this.door);
+    }
+}
