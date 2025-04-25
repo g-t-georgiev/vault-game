2dc41f598315fa49630a6b45fe1c9c0b9c36b69a
Georgi Toni Georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2025-04-26 00:19:08 +0300

    feat: initialized a Timer instance supplying ticker instance from sceneManager's app property.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import SceneManager from './core/SceneManager';
+import Timer from './utils/Timer';
 
 const sceneManager = new SceneManager();
+Timer.getInstance(sceneManager.app.ticker);
 
 await sceneManager.switchScene('VaultClosed');
