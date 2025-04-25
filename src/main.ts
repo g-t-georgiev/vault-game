4f7d95050f47d8e186e6179c66a7bfa911e766a2
Georgi Toni Georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2025-04-26 00:48:05 +0300

    fix: initialized timer ticker with options.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,6 @@ import SceneManager from './core/SceneManager';
 import Timer from './utils/Timer';
 
 const sceneManager = new SceneManager();
-Timer.getInstance(sceneManager.app.ticker);
+Timer.config.ticker = sceneManager.app.ticker;
 
 await sceneManager.switchScene('VaultClosed');
