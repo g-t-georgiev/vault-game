e6256822ff379f78a711e915d0e6489bfbb2660b
Georgi Toni Georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2025-04-25 23:42:34 +0300

    fix: enhanced singleton pattern logic

--- src/utils/Timer.ts
+++ src/utils/Timer.ts
@@ -1,16 +1,22 @@
 import { Ticker } from 'pixi.js';
 
-let timer!:Timer;
-
 export default class Timer {
+
+    private static _instance?: Timer;
+
+    static getInstance(ticker: Ticker): Timer {
+        if (!this._instance) {
+            this._instance = new this(ticker);
+        }
+        return this._instance;
+    }
 
     private ticker!: Ticker;
     private callbacks: Set<(elapsedTime: number) => void> = new Set();
     private running: boolean = false;
-    private elapsedS: number = 0;
+    private elapsedSeconds: number = 0;
     
-    constructor(ticker: Ticker) {
-        if (timer) return timer;
+    private constructor(ticker: Ticker) {
         this.ticker = ticker;
     }
 
@@ -25,26 +31,31 @@ export default class Timer {
     removeUpdateCallback(callback: (elapsedTime: number) => void) {
         if (this.callbacks.has(callback)) {
             this.callbacks.delete(callback);
-            if (this.callbacks.size === 0) this.running = false;
+            if (this.callbacks.size === 0) 
+                this.ticker.remove(this.update, this);
         }
     }
 
     start(): void {
         if (this.running || this.callbacks.size === 0) return;
+        this.elapsedSeconds = 0;
         this.running = true;
     }
 
     resume(): void {
+        if (this.running) return;
         this.running = true;
     }
 
     pause(): void {
+        if (!this.running) return;
         this.running = false;
     }
 
     stop(): void {
-        this.running = false;
-        this.elapsedS = 0;
+        if (!this.running) return;
+        this.pause();
+        this.elapsedSeconds = 0;
     }
 
     destroy(): void {
@@ -55,7 +66,7 @@ export default class Timer {
 
     private update(): void {
         if (!this.running || this.callbacks.size === 0) return;
-        this.elapsedS += this.ticker.elapsedMS * 1000;
-        this.callbacks.forEach(callback => callback(this.elapsedS));
+        this.elapsedSeconds += this.ticker.elapsedMS * 1000;
+        this.callbacks.forEach(callback => callback(this.elapsedSeconds));
     }
 }
