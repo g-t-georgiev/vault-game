04db17ab64a426a440b18ac29f6c5c6675b698a9
Georgi Toni Georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2025-04-26 21:46:42 +0300

    BREAKING CHANGE: call a single "Game" scene which contains all the logic.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,4 @@
 import SceneManager from './core/SceneManager';
-import Timer from './utils/Timer';
 
 const sceneManager = new SceneManager();
-Timer.config.ticker = sceneManager.app.ticker;
-
-await sceneManager.switchScene('VaultClosed');
+await sceneManager.switchScene('Game');
