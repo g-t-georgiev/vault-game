ca9686d8b85f0a524c468820204691441f34676f
Georgi Toni Georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2025-04-27 19:24:24 +0300

    feat: introduced vault lock class and refactored earlier logic with the integrated vault lock initialization, reset and unlock handling

--- src/states/vault/Locked.ts
+++ src/states/vault/Locked.ts
@@ -7,10 +7,7 @@ import Door from '../../prefabs/DoorClosed';
 import Handle from '../../prefabs/DoorHandle';
 import Button from '../../prefabs/Button';
 
-import { PRNG } from '../../utils/prng';
-
-type HandleRotationDirection = 'clockwise' | 'counterclockwise';
-type VaultOpenCombination = [displacement: number, direction: HandleRotationDirection][];
+import VaultLock from '../../prefabs/VaultLock';
 
 // const R2D = 180 / Math.PI;
 const D2R = Math.PI / 180;
@@ -22,13 +19,10 @@ export class Locked extends State {
     private handle!: Handle;
     private rotateHandleBtns!: Container;
 
-    private prng!: PRNG;
-    private vaultOpenCombination!: VaultOpenCombination;
+    private vaultLock!: VaultLock;
 
     constructor(utils: IStateUtils) {
         super(utils);
-
-        this.prng = new PRNG();
 
         this.door = new Door(Assets.get('door'));
         this.door.anchor.set(0.5);
@@ -48,40 +42,42 @@ export class Locked extends State {
         rotateHandleToRightBtn.x += 290;
         this.rotateHandleBtns.addChild(rotateHandleToLeftBtn, rotateHandleToRightBtn);
 
+        this.vaultLock = new VaultLock({
+            onInit: () => {
+                console.log('VAULT_LOCK_INIT');
+                this.handle.rotation = 0;
+            },
+            onReset: () => {
+                console.log('VAULT_LOCK_RESET');
+                this.handle.rotation = 0;
+            },
+            onUnlock: () => {
+                console.log('VAULT_LOCK_UNLOCKED');
+                this.utils.requestStateChange('Unlocked');
+            }
+        });
+
         this.addChild(this.door, this.rotateHandleBtns);
     }
 
     load(): void {
-
-        this.generateOpenCombination();
+        this.vaultLock.isUnlocked ? this.vaultLock.reset() : this.vaultLock.init();
     }
 
-    private generateOpenCombination() {
-        this.vaultOpenCombination = [];
-        for (let i = 0; i < 3; i++) {
-            let displacement = Math.floor(this.prng.next() * 9) + 1;
-            let direction: HandleRotationDirection = this.prng.next() < 0.5 ? 'clockwise' : 'counterclockwise';
-            this.vaultOpenCombination.push([displacement, direction]);
-        }
-        console.log(
-            '%c [SECRET COMBINATION]', 'color:#ff0000',
-            this.vaultOpenCombination.map((step) => step.join(' ')).join(', ')
-        );
+    private async rotateHandleCounterClockwise() {
+        await this.rotateHandle(-ROTATION_STEP);
+        this.vaultLock.tryToUnlock(1, 'counterclockwise');
     }
 
-    private rotateHandleCounterClockwise() {
-        console.log(this.vaultOpenCombination);
-        this.rotateHandle(-ROTATION_STEP);
+    private async rotateHandleClockwise() {
+        await this.rotateHandle(ROTATION_STEP);
+        this.vaultLock.tryToUnlock(1, 'clockwise');
     }
 
-    private rotateHandleClockwise() {
-        this.rotateHandle(ROTATION_STEP);
-    }
-
-    private rotateHandle(step: number) {
+    private async rotateHandle(step: number) {
         const currentRotation = this.handle.rotation;
         const targetRotation = Math.round((currentRotation + step) / ROTATION_STEP) * ROTATION_STEP;
-        gsap.to(this.handle, { 
+        return gsap.to(this.handle, { 
             rotation: targetRotation, 
             duration: 0.3, 
             ease: 'power2.out', 
