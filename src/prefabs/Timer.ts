4dc50ba819d3889560345e01419ff37aeb5db428
Georgi Toni Georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2025-04-27 23:02:04 +0300

    feat: created timer class

---
+++ src/prefabs/Timer.ts
@@ -0,0 +1,77 @@
+import { Container, Graphics, Text } from 'pixi.js';
+
+export default class Timer extends Container {
+
+    private static instance: Timer | null = null;
+    public static getInstance() {
+        if (!this.instance) {
+            this.instance = new this();
+        }
+        return this.instance;
+    }
+    public static removeInstance() {
+        this.instance?.destroy();
+        this.instance = null;
+    }
+
+    protected time: number = 0;
+    protected isRunning = false;
+
+    private timerPad!: Graphics;
+    private timerText!: Text;
+
+    constructor() {
+        super();
+
+        this.timerText = new Text(this.formatTime(this.time), {
+            fontFamily: 'Share Tech Mono',
+            fontWeight: '600',
+            fontSize: 55,
+            lineHeight: 1,
+            fill: 0xff0000
+        });
+        this.timerText.scale.set(1, 1.35);
+        this.timerText.position.set(5, 5);
+
+        this.timerPad = new Graphics();
+        this.timerPad.beginFill(0x000000);
+        this.timerPad.drawRoundedRect(0, 0, this.timerText.width + 5, this.timerText.height + 5, 5);
+        this.timerPad.endFill();
+
+        this.addChild(this.timerPad, this.timerText);
+    }
+
+    reset() {
+        this.time = 0;
+        this.timerText.text = this.formatTime(this.time);
+    }
+
+    start() {
+        this.isRunning = true;
+    }
+
+    stop() {
+        this.isRunning = false;
+    }
+
+    update(elapsedMS: number) {
+        if (this.isRunning) {
+            this.time += elapsedMS;
+            this.timerText.text = this.formatTime(this.time);
+        }
+    }
+
+    private formatTime(ms: number) {
+
+        const totalSeconds = Math.floor(ms / 1000);
+        const hours = Math.floor(totalSeconds / 3600);
+        const minutes = Math.floor((totalSeconds % 3600) / 60);
+        const seconds = totalSeconds % 60;
+
+        const paddedHours = String(hours).padStart(2, '0');
+        const paddedMinutes = String(minutes).padStart(2, '0');
+        const paddedSeconds = String(seconds).padStart(2, '0');
+    
+        return `${paddedHours}:${paddedMinutes}:${paddedSeconds}`;
+    }
+}
