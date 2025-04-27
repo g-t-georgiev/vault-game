e7a6021f0b92fbb4413ed023d87f440f1c7f096f
Georgi Toni Georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2025-04-27 17:19:02 +0300

    feat: added a base state class, type and interfaces

---
+++ src/core/State.ts
@@ -0,0 +1,21 @@
+import { Container } from 'pixi.js';
+
+export interface IState {
+    load(): Promise<void> | void
+    unload(): Promise<void> | void
+    update(elapsedMS: number): void
+}
+
+export interface IStateUtils {
+    requestStateChange: (state: string) => void
+}
+
+export type BaseState = Container & Partial<IState>
+
+export default abstract class State extends Container {
+
+    constructor(protected utils: IStateUtils) {
+        super();
+    }
+
+}
