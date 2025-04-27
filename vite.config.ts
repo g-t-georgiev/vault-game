95991ca19989ee5a8edb70defd518ba96d3f0092
Georgi Toni Georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2025-04-28 01:03:39 +0300

    feat: used backup from NODE process environments

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import { defineConfig, loadEnv } from "vite";
 export default defineConfig(({ mode }) => {
 
 	const env = loadEnv(mode, process.cwd(), '');
+	const BASE_URL = env.VITE_BASE_URL ?? (process.env.NODE_ENV == 'production' ? '/vault-game/' : '/');
 
 	return {
-		base: env.VITE_BASE_URL == 'production' ? '/vault-game/' : '/',
+		base: BASE_URL,
 		build: {
 			target: "esnext",
 			rollupOptions: {
