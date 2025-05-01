ca8b2105ffa303d902d74f2ae5abc9d2c0bdc1bd
Georgi Toni Georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2025-05-02 02:06:59 +0300

    fix: simplified glitter animation timeline

--- src/prefabs/Glitter.ts
+++ src/prefabs/Glitter.ts
@@ -1,4 +1,4 @@
-import { Sprite, Texture, BLEND_MODES } from 'pixi.js';
+import { Sprite, Texture } from 'pixi.js';
 import gsap from 'gsap';
 
 export default class Glitter extends Sprite {
@@ -6,74 +6,42 @@ export default class Glitter extends Sprite {
 
     constructor(texture: Texture) {
         super(texture);
-        this.alpha = 0;
         this.anchor.set(0.5);
-        this.blendMode = BLEND_MODES.ADD;
-        this.animationTl = null;
     }
 
     playAnimation() {
-        this.alpha = 0;
-        const initialScale = 0.5 + Math.random() * 0.5;
-        this.scale.set(initialScale);
-        this.rotation = Math.random() * Math.PI * 2;
-
-        const tl = gsap.timeline({
-            paused: true,
-            repeat: -1,
-            repeatDelay: 1 + Math.random(),
-            defaults: { ease: 'sine.inOut' },
-        });
-
-        tl.to(this, {
-            alpha: 1,
-            duration: 0.6,
-        })
-            .to(
-                this,
-                {
-                    rotation: `+=${0.3 + Math.random() * 0.3}`,
-                    duration: 0.5,
-                },
-                '<'
-            )
-            .to(
-                this.scale,
-                {
-                    x: initialScale + 0.2,
-                    y: initialScale + 0.2,
-                    duration: 0.5,
-                },
-                '<'
-            )
-            .to(
-                this,
-                {
-                    alpha: 0.7,
-                    duration: 0.35,
-                    yoyo: true,
-                    repeat: 1,
-                },
-                '<+0.3'
-            )
-            .to(
-                this,
-                {
-                    alpha: 0,
-                    duration: 1,
-                },
-                '>0.1'
-            );
-
-        const initialDelay = Math.random() * 1;
-        gsap.delayedCall(initialDelay, () => tl.play());
 
         if (this.animationTl) {
             this.animationTl.kill();
-            this.animationTl = null;
         }
 
-        this.animationTl = tl;
+        this.animationTl = gsap.timeline({
+            paused: true,
+            repeat: -1,
+            repeatDelay: 1 + Math.random(),
+            yoyo: true,
+            defaults: { ease: 'power2.out' },
+        })
+        .set(this.scale, {
+            x: 0,
+            y: 0
+        })
+        .set(this, {
+            rotation: Math.random() * (Math.PI * 2)
+        })
+        .to(this.scale, {
+            x: 1,
+            y: 1,
+            duration: 0.5,
+        })
+        .to(this, {
+            rotation: `+=${Math.random() * 0.3 + 0.3}`,
+            duration: 0.5,
+        }, '<');
+
+        const initialDelay = Math.random() * 1;
+        gsap.delayedCall(initialDelay, () => this.animationTl?.play());
+
         return this.animationTl;
     }
 
