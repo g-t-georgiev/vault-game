efcfb5c00c26b1de94b8aedd14f700216d03c4ab
Georgi Toni Georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2025-05-03 14:59:39 +0300

    feat: changed `onResize` method parameters to an object with several properties, such as screen width and height, browser window width and height, device css pixels resolution and screen orientation and created an interface for it

--- src/core/Scene.ts
+++ src/core/Scene.ts
@@ -1,15 +1,25 @@
 import { Container } from 'pixi.js';
 import type { SceneUtils } from './SceneManager';
 
+export interface ISceneResizeParams {
+    screenWidth: number,
+    screenHeight: number,
+    resolution: number,
+    deviceWidth: number,
+    deviceHeight: number,
+    deviceOrientation: ScreenOrientation
+}
+
 export interface Scene {
     load?(): void | Promise<void>;
     unload?(): void | Promise<void>;
     start?(): void | Promise<void>;
-    onResize?(width: number, height: number): void;
+    onResize?(params: ISceneResizeParams): void;
     update?(delta: number): void;
 }
 
 export abstract class Scene extends Container {
+
     abstract name: string;
 
     constructor(protected utils: SceneUtils) {
