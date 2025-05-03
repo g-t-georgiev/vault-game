3fd8a660c7db54b5afd9bd5a14b7462d86c479a7
Georgi Toni Georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2025-05-03 15:04:55 +0300

    feat: changed passed params to the `resize` to an object implementing the ISceneResizeParams interface

--- src/prefabs/Background.ts
+++ src/prefabs/Background.ts
@@ -1,14 +1,16 @@
 import { DisplayObject, Sprite, Texture } from 'pixi.js';
 import { centerObjects } from '../utils/misc';
 
+import { ISceneResizeParams } from '../core/Scene';
+
 export default class Background extends Sprite {
     constructor(texture: Texture) {
         super(texture);
     }
 
-    resize(width: number, height: number) {
-        const scaleByWidth = width / this.texture.width;
-        const scaleByHeight = height / this.texture.height;
+    resize(params: ISceneResizeParams) {
+        const scaleByWidth = params.screenWidth / this.texture.width;
+        const scaleByHeight = params.screenHeight / this.texture.height;
         const scaleFactor = Math.min(scaleByWidth, scaleByHeight);
         this.width = this.texture.width * scaleFactor;
         this.height = this.texture.width * scaleFactor;
