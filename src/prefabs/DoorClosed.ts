71fdc9737dd3ed75af52edc1daed67318e662f69
Georgi Toni Georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2025-05-10 19:57:24 +0300

    feat: added `fadeIn` and `fadeOut` methods and moved set anchor on initialization

--- src/prefabs/DoorClosed.ts
+++ src/prefabs/DoorClosed.ts
@@ -1,8 +1,20 @@
 import { Sprite, Texture } from 'pixi.js';
+import gsap from 'gsap';
 
 export default class DoorClosed extends Sprite {
 
     constructor(texture: Texture) {
         super(texture);
+        this.anchor.set(0.5);
+    }
+
+    async fadeOut() {
+        if (this.alpha === 0) return;
+        return gsap.to(this, { alpha: 0, duration: 0.3, ease: 'power2.out' });
+    }
+
+    async fadeIn() {
+        if (this.alpha === 1) return;
+        return gsap.to(this, { alpha: 1, duration: 0.3, ease: 'power2.out' });
     }
 }
