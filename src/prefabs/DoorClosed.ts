c08419c80bd835ffefaadd18883b378a428c15ec
Georgi Toni Georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2025-05-10 20:13:33 +0300

    fix: changed fade-in and -out duration values

--- src/prefabs/DoorClosed.ts
+++ src/prefabs/DoorClosed.ts
@@ -1,6 +1,9 @@
 import { Sprite, Texture } from 'pixi.js';
 import gsap from 'gsap';
 
+const FADE_IN_DURATION = 0.7;
+const FADE_OUT_DURATION = 0.3;
+
 export default class DoorClosed extends Sprite {
 
     constructor(texture: Texture) {
@@ -8,13 +11,13 @@ export default class DoorClosed extends Sprite {
         this.anchor.set(0.5);
     }
 
-    async fadeOut() {
-        if (this.alpha === 0) return;
-        return gsap.to(this, { alpha: 0, duration: 0.3, ease: 'power2.out' });
-    }
-
     async fadeIn() {
         if (this.alpha === 1) return;
-        return gsap.to(this, { alpha: 1, duration: 0.3, ease: 'power2.out' });
+        return gsap.to(this, { alpha: 1, duration: FADE_IN_DURATION, ease: 'power2.out' });
+    }
+
+    async fadeOut() {
+        if (this.alpha === 0) return;
+        return gsap.to(this, { alpha: 0, duration: FADE_OUT_DURATION, ease: 'power2.out' });
     }
 }
--- src/prefabs/DoorOpened.ts
+++ src/prefabs/DoorOpened.ts
@@ -1,6 +1,9 @@
 import { Assets, Sprite, Container, DisplayObject } from 'pixi.js';
 import gsap from 'gsap';
 
+const FADE_IN_DURATION = 0.3;
+const FADE_OUT_DURATION = 0.7;
+
 export default class DoorOpened extends Container {
     
     private door!: Sprite;
@@ -20,14 +23,13 @@ export default class DoorOpened extends Container {
         );
     }
 
-    async fadeOut() {
-        if (this.alpha === 0) return;
-        return gsap.to(this, { alpha: 0, duration: 0.3, ease: 'power2.out' });
-    }
-
     async fadeIn() {
         if (this.alpha === 1) return;
-        return gsap.to(this, { alpha: 1, duration: 0.3, ease: 'power2.out' });
+        return gsap.to(this, { alpha: 1, duration: FADE_IN_DURATION, ease: 'power2.out' });
     }
 
+    async fadeOut() {
+        if (this.alpha === 0) return;
+        return gsap.to(this, { alpha: 0, duration: FADE_OUT_DURATION, ease: 'power2.out' });
+    }
 }
